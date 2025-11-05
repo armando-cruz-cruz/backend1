35c48fa708d4da6a9cace87f8c7634065319b8fc
armando-cruz-cruz <armando.cruz@uvmnet.edu>
2025-11-05 17:14:42 -0600

    Agregar el puerto como variable de ambiente

--- index.js
+++ index.js
@@ -76,6 +76,7 @@ const unknownEndpoint = (request, respose) => {
     respose.status(404).send({error: 'unknown endpoint'})
 }
 app.use(unknownEndpoint)
+// Para el proveedor le damos la opcion de que elija el puerto
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () =>{
     console.log(`Server running on port ${PORT}`);
